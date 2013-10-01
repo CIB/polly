2d4cad479e093e5bccb34f7ebe97024f34f34564
Florian Sattler <vuld3r@gmail.com>
2013-10-01 23:41:00 +0200

    added check for dims to be fixed

--- lib/Analysis/ScopStatistics.cpp
+++ lib/Analysis/ScopStatistics.cpp
@@ -38,6 +38,7 @@ bool ScopStatistics::runOnScop(Scop &S) {
     outs() << ">>>>>>>>> Run on Scop: "  << S.getNameStr() << "\n";
     Dependences *DE = &getAnalysis<Dependences>();
     isl_union_map *m = DE->getDependences(Dependences::TYPE_ALL); 
+    int i;
     outs() << "Map dump:\n"; isl_union_map_dump(m);
     //isl_union_map_n_ stuff
     bool* p = (bool *) malloc(10*sizeof(bool));//10 to replace with number of maps
@@ -49,9 +50,20 @@ bool ScopStatistics::runOnScop(Scop &S) {
     isl_union_map_foreach_map(m, testOnMap, &s);
 
     outs() << "\n ----------------- \n";
-
     outs() << "Some nParam: " << s.nparam << "\n";
-    
+   
+    outs() << "\n ----------------- \n";
+    outs() << "uniform output \n";
+    outs() << mup->nMaps << "were found \n";
+    for(i = 0,i < mup->nMaps; i++){
+      if(mup->p[i] == true) {
+        outs() << "true ";
+      } else {
+        outs() << "false ";
+      }
+    }
+    outs() << "\n";
+
     outs() << "End of my output \n";
     
 
@@ -68,20 +80,41 @@ bool ScopStatistics::runOnScop(Scop &S) {
   }
 
   int workOnMap(__isl_take isl_map *map, void *user) {
+    int i;
+    bool earlyStop = false;
+    isl_int iInt;
+    
+    isl_int_init(iInt);
+
     MapUniform* mapU = (MapUniform *) user;
-    (mapU->nMaps)++;
     //isl_set* setFmap = isl_set_from_map(map);
     isl_set* setFdeltas = isl_map_deltas(map);
     //isl_dim* dimFdeltas = isl_set_get_dim(setFdeltas);  
-    isl_int* iInt;
 
     // anzahl der dims 
 
 //dim type dim out dim_all
 
-    //isl_set_fast_dim_is_fixed(setFdeltas, 0, iInt);
-    // return bool/int 
-    isl_set_plain_is_fixed(setFdeltas,isl_dim_type,0,iInt);
+    // warscheinlich i isl_int 
+    // isl_int_set_si(iInt,0) , () , isl_int_add_ui(iInt,1)
+    for(i = 0;i < 10;i++) {
+      //isl_set_fast_dim_is_fixed(setFdeltas, 0, iInt);
+      // return bool/int 
+      // isl_int_set_si(iInt,1);
+      if(isl_set_fast_dim_is_fixed(setFdeltas, isl_dim_all, &iInt) == 0) {
+        mapU->p[mapU->nMaps] = false;
+        mapU->nMaps++;
+        earlyStop = true;
+        break;      
+      }
+    }
+    if(!earlyStop) {
+      mapU->p[mapU->nMaps] = true;
+      mapU->nMaps++;
+    }
+
+    //isl_set_plain_is_fixed(setFdeltas,isl_dim_type,0,iInt);
+    isl_int_clear(iInt);
     return 0;
   }
 
