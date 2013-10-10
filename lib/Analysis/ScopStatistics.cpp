6b1a70197daded0c38c26eb65c2029992f26b12d
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2013-10-10 15:49:31 +0200

    Clang Format & isl memory management fixes

--- lib/Analysis/ScopStatistics.cpp
+++ lib/Analysis/ScopStatistics.cpp
@@ -29,124 +29,105 @@ public:
 
 MapUniform::MapUniform() {
   this->nMaps = 0;
-  //this->p = bp;
+  // this->p = bp;
 }
 
 int workOnMap(__isl_take isl_map *map, void *user);
 int testOnMap(__isl_take isl_map *map, void *user);
 
 bool ScopStatistics::runOnScop(Scop &S) {
-    outs() << ">>>>>>>>> Run on Scop: "  << S.getNameStr() << "\n";
-    Dependences *DE = &getAnalysis<Dependences>();
-    isl_union_map *m = DE->getDependences(Dependences::TYPE_ALL); 
-    int i;
-    outs() << "Map dump:\n"; isl_union_map_dump(m);
-    //isl_union_map_n_ stuff
-    //int j = isl_union_map_n_map(m);
-    //bool* p = (bool *) malloc(j*sizeof(bool));//10 to replace with number of maps
-    MapUniform* mup = new MapUniform();
+  outs() << ">>>>>>>>> Run on Scop: " << S.getNameStr() << "\n";
+  Dependences *DE = &getAnalysis<Dependences>();
+  isl_union_map *m = DE->getDependences(Dependences::TYPE_ALL);
+  int i;
+  MapUniform *mup = new MapUniform();
 
-    isl_union_map *newUmap = isl_union_map_copy(m);
-    isl_union_map_foreach_map(newUmap, workOnMap, &mup);
+  isl_union_map_foreach_map(m, workOnMap, &mup);
 
-    outs() << "\n ----------------- \n";
-    outs() << "Some nParam: " << s.nparam << "\n";
-   
-    outs() << "\n ----------------- \n";
-    outs() << "uniform output \n";
-    outs() << mup->nMaps << "were found \n";
-    for(i = 0;i < mup->nMaps; i++){
-      if(mup->p[i] == true) {
-        outs() << "true ";
-      } else {
-        outs() << "false ";
-      }
+  outs() << "\n ----------------- \n";
+  outs() << "Some nParam: " << s.nparam << "\n";
+
+  outs() << "\n ----------------- \n";
+  outs() << "uniform output \n";
+  outs() << mup->nMaps << "were found \n";
+  for (i = 0; i < mup->nMaps; i++) {
+    if (mup->p[i] == true) {
+      outs() << "true ";
+    } else {
+      outs() << "false ";
     }
-    outs() << "\n";
-
-    isl_union_map_free(newUmap);
-    return false;
   }
+  outs() << "\n";
 
-  void ScopStatistics::getAnalysisUsage(AnalysisUsage &AU) const {
-    AU.addRequired<Dependences>();
-    AU.addRequired<ScopInfo>();
-    AU.setPreservesAll();
-  }
+  isl_union_map_free(m);
+  return false;
+}
 
-  int workOnMap(__isl_take isl_map *map, void *user) {
-    unsigned i,j;
-    isl_int iInt;
-    isl_map *newMap;
-    isl_int_init(iInt);
+void ScopStatistics::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequired<Dependences>();
+  AU.addRequired<ScopInfo>();
+  AU.setPreservesAll();
+}
 
-    MapUniform* mapU = (MapUniform *) user;
+int workOnMap(__isl_take isl_map *map, void *user) {
+  unsigned i, j;
+  isl_int iInt;
+  isl_map *newMap;
+  isl_int_init(iInt);
 
+  MapUniform *mapU = (MapUniform *)user;
 
-    outs() << "IN " << isl_map_dim(map,isl_dim_in) << "OUT " << isl_map_dim(map,isl_dim_out) << "\n";
+  outs() << "IN " << isl_map_dim(map, isl_dim_in) << "OUT "
+         << isl_map_dim(map, isl_dim_out) << "\n";
 
-    int in = isl_map_dim(map,isl_dim_in);
-    int out = isl_map_dim(map,isl_dim_out); 
+  int in = isl_map_dim(map, isl_dim_in);
+  int out = isl_map_dim(map, isl_dim_out);
 
-    outs() << "MAP DUMP\n";
-    isl_map_dump(map);
-    outs() << "\n";
-    if(in != out) {
-      mapU->p.push_back(false);
-      mapU->nMaps++;
-      isl_int_clear(iInt);
-      return 0; 
-    }
-
-    //isl_set* setFmap = isl_set_from_map(map);
-    newMap = isl_map_copy(map);
-    //isl_space_set_dim_id(); 
-    newMap = isl_map_reset_tuple_id(newMap,isl_dim_in); 
-    newMap = isl_map_reset_tuple_id(newMap,isl_dim_out); 
-    
-    
-    
-    
-    
-    
-    isl_set* setFdeltas = isl_map_deltas(newMap);
-    //isl_dim* dimFdeltas = isl_set_get_dim(setFdeltas);  
-
-    // anzahl der dims 
-
-    //dim type dim out dim_all
-    j = isl_set_dim(setFdeltas, isl_dim_all);
-
-    outs() << "isl_set_dims: " << j << "\n";
-    for(i = 0;i < j;i++) {
-      //isl_set_fast_dim_is_fixed(setFdeltas, 0, iInt);
-      isl_int_set_si(iInt,i);
-      if(!isl_set_fast_dim_is_fixed(setFdeltas, isl_dim_all, &iInt)) {
-        mapU->p.push_back(false);
-        mapU->nMaps++;
-        isl_int_clear(iInt);
-        return 0;      
-      }
-    }
-    mapU->p.push_back(true);
-    mapU->nMaps++;
-
-    //isl_set_plain_is_fixed(setFdeltas,isl_dim_type,0,iInt);
+  //outs() << "MAP DUMP\n";
+  //isl_map_dump(map);
+  //outs() << "\n";
+  if (in != out) {
+  //  mapU->p.push_back(false);
+  //  mapU->nMaps++;
+    isl_map_free(map);
     isl_int_clear(iInt);
     return 0;
   }
 
-  int testOnMap(__isl_take isl_map *map, void *user) {
-      mapSave* mapS = (mapSave*) user;
-      mapS->nparam = isl_map_n_param(map);
-      
-    
+  newMap = isl_map_copy(map);
+  newMap = isl_map_reset_tuple_id(newMap, isl_dim_in);
+  newMap = isl_map_reset_tuple_id(newMap, isl_dim_out);
+
+  isl_set *setFdeltas = isl_map_deltas(newMap); 
+  j = isl_set_dim(setFdeltas, isl_dim_all);
+
+  outs() << "isl_set_dims: " << j << "\n";
+  for (i = 0; i < j; i++) {
+    if (!isl_set_fast_dim_is_fixed(setFdeltas, i, &iInt)) {
+      outs() << "constant dim: " << i;
+      outs() << "\n";
+      mapU->p.push_back(false);
+      mapU->nMaps++;
+      isl_map_free(map);
+      isl_set_free(setFdeltas);
+      isl_int_clear(iInt);
       return 0;
     }
+  }
+  mapU->p.push_back(true);
+  mapU->nMaps++;
 
-    char ScopStatistics::ID = 0;
+  isl_map_free(map);
+  isl_set_free(setFdeltas);
+  isl_int_clear(iInt);
+  return 0;
+}
 
-    Pass *polly::createScopStatisticsPass() {return new ScopStatistics(); }
+char ScopStatistics::ID = 0;
 
-  INITIALIZE_PASS_BEGIN(ScopStatistics, "polly-stat","Polly - get stats", false, false);
-  INITIALIZE_PASS_END(ScopStatistics, "polly-stat","Polly - get stats", false, false);
+Pass *polly::createScopStatisticsPass() { return new ScopStatistics(); }
+
+INITIALIZE_PASS_BEGIN(ScopStatistics, "polly-stat", "Polly - get stats", false,
+                      false);
+INITIALIZE_PASS_END(ScopStatistics, "polly-stat", "Polly - get stats", false,
+                    false);
