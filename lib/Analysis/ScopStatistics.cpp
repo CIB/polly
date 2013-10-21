1117d8b6e81b2c6badee7cb6cba9848150ba4574
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2013-10-21 12:34:09 +0200

    Fix Uniform/Affine map generation

--- lib/Analysis/ScopStatistics.cpp
+++ lib/Analysis/ScopStatistics.cpp
@@ -1,7 +1,8 @@
 #define DEBUG_TYPE "polly-stat"
+#include "llvm/Support/Debug.h"
 
 #include "llvm/ADT/Statistic.h"
-#include "llvm/Support/Debug.h"
+
 #include "polly/ScopStatistics.h"
 #include "polly/ScopPass.h"
 #include "polly/ScopInfo.h"
@@ -20,6 +21,7 @@ STATISTIC(UniformScops, "Number of the Scops witch are uniform");
 STATISTIC(AffineScops, "Number of the Scops witch are affine");
 STATISTIC(UniformMaps, "Number of the Maps witch are uniform");
 STATISTIC(AffineMaps, "Number of the Maps witch are affine");
+STATISTIC(NoDeps, "Number of Scops without dependencies");
 
 class MapUniform {
 
@@ -29,45 +31,45 @@ public:
   MapUniform();
 };
 
-MapUniform::MapUniform() {
-  this->nMaps = 0;
-}
+MapUniform::MapUniform() { this->nMaps = 0; }
 
 int workOnMap(__isl_take isl_map *map, void *user);
 
-bool output;
-
 bool ScopStatistics::runOnScop(Scop &S) {
-  // set output to true for debug info
-  output = false;
+  DEBUG(dbgs() << "Running on: " << S.getNameStr() << "\n");
 
   int i;
   Dependences *DE = &getAnalysis<Dependences>();
   isl_union_map *m = DE->getDependences(Dependences::TYPE_ALL);
   MapUniform *mup = new MapUniform();
-  
-  if(output)
-    outs() << "Start working on union_map\n";
-  isl_union_map_foreach_map(m, workOnMap, mup);
-  for(i=0; i < mup->nMaps; i++) {
-    if(mup->p[i] == false)
-      ++AffineScops;
-    if(i == ((mup->nMaps)-1))
-      ++UniformScops;
+
+  int mapCount = isl_union_map_n_map(m);
+  if (!mapCount) {
+    NoDeps++;
+    isl_union_map_free(m);
+    return false;
   }
 
-  if(output) {
-    outs() << "Uniform output \n";
-    outs() << mup->nMaps << " were found on this union_map \n";
-    for (i = 0; i < mup->nMaps; i++) {
-      if (mup->p[i] == true) {
-        outs() << "True ";
-      } else {
-        outs() << "False ";
-      }
-    }
-    outs() << "\n";
+  DEBUG(dbgs() << "Start working on union_map\n");
+  isl_union_map_foreach_map(m, workOnMap, mup);
+
+  bool isUniform = true;
+  for (i = 0; i < mup->nMaps; i++) {
+    isUniform = isUniform && mup->p[i];
+    if (!isUniform)
+      break;
   }
+   
+  if(isUniform)
+      ++UniformScops;
+  else 
+      ++AffineScops;
+
+  DEBUG(dbgs() << "Uniform output \n");
+  DEBUG(dbgs() << mup->nMaps << " were found on this union_map \n");
+  DEBUG(for (i = 0; i < mup->nMaps; i++) dbgs()
+            << ((mup->p[i]) ? "True " : "False ");
+        dbgs() << "\n";);
 
   isl_union_map_free(m);
   return false;
@@ -85,20 +87,18 @@ int workOnMap(__isl_take isl_map *map, void *user) {
   isl_map *newMap;
   isl_int_init(iInt);
 
-  if(output)
-    outs() << "----------\n";
+  DEBUG(dbgs() << "----------\n");
   MapUniform *mapU = (MapUniform *)user;
 
-  if(output)
-    outs() << "IN " << isl_map_dim(map, isl_dim_in) << "OUT "
-         << isl_map_dim(map, isl_dim_out) << "\n";
+  DEBUG(dbgs() << "IN " << isl_map_dim(map, isl_dim_in) << "OUT "
+               << isl_map_dim(map, isl_dim_out) << "\n");
 
   int in = isl_map_dim(map, isl_dim_in);
   int out = isl_map_dim(map, isl_dim_out);
 
   if (in != out) {
     mapU->p.push_back(false);
-    mapU->nMaps = (mapU->nMaps)+1;
+    mapU->nMaps = (mapU->nMaps) + 1;
     isl_map_free(map);
     isl_int_clear(iInt);
     return 0;
@@ -111,24 +111,22 @@ int workOnMap(__isl_take isl_map *map, void *user) {
   isl_set *setFdeltas = isl_map_deltas(newMap);
   j = isl_set_dim(setFdeltas, isl_dim_all);
 
-  if(output)
-    outs() << "isl_set_dims: " << j << "\n";
-  for (i = 0; i < j; i++) {
-    if (!isl_set_fast_dim_is_fixed(setFdeltas, i, &iInt)) {
-      mapU->p.push_back(false);
-      mapU->nMaps = (mapU->nMaps)+1;
-      ++AffineMaps;
-      isl_map_free(map);
-      isl_set_free(setFdeltas);
-      isl_int_clear(iInt);
-      return 0;
-    }
-    if(output)
-      outs() << "constant dim: " << i  << "\n";
+  DEBUG(dbgs() << "isl_set_dims: " << j << "\n");
+  bool isUniformDep = true;
+  i = 0;
+  while (isUniformDep && i < j) {
+    isUniformDep =
+        isUniformDep && isl_set_fast_dim_is_fixed(setFdeltas, i, &iInt);
+    i++;
   }
-  mapU->p.push_back(true);
-  mapU->nMaps = (mapU->nMaps)+1;
-  ++UniformMaps;
+
+  mapU->p.push_back(isUniformDep);
+  mapU->nMaps = (mapU->nMaps) + 1;
+
+  if (isUniformDep)
+    ++UniformMaps;
+  else
+    ++AffineMaps;
 
   isl_map_free(map);
   isl_set_free(setFdeltas);
